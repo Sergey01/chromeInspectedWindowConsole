96b1841be0930d90881a6b253a4270321026f914
Sergey Zeygerman <Sergey01@users.noreply.github.com>
2021-08-12 21:54:54 -0400

    Added getResources functionality

--- js/console.js
+++ js/console.js
@@ -1,5 +1,7 @@
-const pp = (stuff) => JSON.stringify(stuff, null, 2); // pretty print
+// implement pretty print functionality
+const pp = (stuff) => JSON.stringify(stuff, null, 2);
 
+// "console" functionality
 const input = document.getElementById('commandInput');
 const submit = document.getElementById('inputSubmit');
 const output = document.getElementById('outputContainer');
@@ -13,4 +15,20 @@ submit.addEventListener('click', async (e) => {
       output.innerText = pp(result);
     }
   })
-})
+});
+
+// getResources functionality
+const resourcesDiv = document.getElementById('resources');
+const refreshResources = document.getElementById('refreshResources');
+
+// initial resource list grab
+chrome.devtools.inspectedWindow.getResources((resources) => {
+  resourcesDiv.innerText = pp(resources);
+});
+
+// Refresh button functionality
+refreshResources.addEventListener('click', async (e) => {
+  chrome.devtools.inspectedWindow.getResources((resources) => {
+    resourcesDiv.innerText = pp(resources);
+  })
+});
