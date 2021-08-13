ea7f1dfc3df3cdb7245369384c032a9dbc5c12ae
Sergey Zeygerman <Sergey01@users.noreply.github.com>
2021-08-12 23:38:22 -0400

    current tabID now shows on top of devtools panel

--- js/console.js
+++ js/console.js
@@ -1,6 +1,9 @@
 // implement pretty print functionality
 const pp = (stuff) => JSON.stringify(stuff, null, 2);
 
+// grab and display tabId
+document.getElementById('infoTabId').innerText = `TabID: ${chrome.devtools.inspectedWindow.tabId}`;
+
 // "console" functionality
 const input = document.getElementById('commandInput');
 const submit = document.getElementById('inputSubmit');
@@ -25,27 +28,35 @@ const refreshResources = document.getElementById('refreshResources');
 // main function for getting resources. Clears the resourcesTable tbody and 
 // fills in line items from a getResources call
 const getResources = () => {
+  // clear existing table body
   resourcesBody.remove();
   resourcesBody = document.createElement('tbody');
   resourcesBody.id = "resourcesBody";
   resourcesTable.append(resourcesBody);
+
+  // fetch inspectedWindow resources
   chrome.devtools.inspectedWindow.getResources((resources) => {
     resources.forEach(resource => {
+      // parsing each resource into a table row
       const resourceItem = document.createElement('tr');
       const resourceType = document.createElement('td');
       const resourceURL = document.createElement('td');
       resourceItem.className = "resourceRow";
       resourceType.className = "resourceType";
-      resourceType.innerText = JSON.stringify(resource.type).slice(1,-1);
+      resourceType.innerText = resource.type;
       resourceURL.className = "resourceURL";
-      resourceURL.innerText = JSON.stringify(resource.url).slice(1,-1);
+      resourceURL.innerText = resource.url;
+
+      // appending resource type and url to the row
       resourceItem.append(resourceType, resourceURL);
+
+      // appending the row to the table body
       resourcesBody.append(resourceItem);
     });
   });
 };
 
-// initial resource list grab
+// initial resource table render
 getResources();
 
 // Refresh button functionality
